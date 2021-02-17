c3648924b9493681bcec3d58535ea2d774ae461c
mpalomera <miguel.palomera@tec.mx>
2021-02-17 03:04:56 +0000

    setup

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 
-int main(){
-  printf("Hello world\n");
+int main(int numarg, char **argv){
+  for(int i = 1; i < numarg; i++){
+    printf("Hello world %s\n",argv[i]);
+  }
+  
   return 0;
 }
